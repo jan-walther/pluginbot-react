bac1ad3053a7bfe6c94757204875854541e84ac3
jan <62257247+jan-walther@users.noreply.github.com>
2020-04-21 19:08:48 +0200

    Remove sample

--- example/simple/view/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function(props){
-    return <div>
-        <h1>Pet Adoption agency</h1>
-        <br/>
-        <p>Find a best friend!</p>
-    </div>
-}
